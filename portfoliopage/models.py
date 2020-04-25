66753bf9b180c579e65dec89cfc98c57d289710c
unknown <gregor.boltz@code.berlin>
2020-04-25 20:29:49 +0200

    started on connecting form with db

--- portfoliopage/models.py
+++ portfoliopage/models.py
@@ -4,7 +4,7 @@ class Work(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(200), nullable=False)
     date = db.Column(db.String(200), nullable=False,)
-    image = db.Column(db.String(20), nullable=False)
+    image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
     description = db.Column(db.Text, nullable=False, )
 
     def __repr__(self):
